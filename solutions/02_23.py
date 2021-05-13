24ca5e3ae0a65b823a60dfc823af4a83e38fbd52
Mike Metzger <mike@flexiblecreations.com>
2021-05-13 13:04:11 -0500

    - Added rows w/ full NaN values to penguins.csv - Added duplicate rows to penguins.csv - Modified content of notebook 2 to reference updated dataset - Modified solution 02_23 to remove mention of "no change"

--- solutions/02_23.py
+++ solutions/02_23.py
@@ -1,7 +1,2 @@
 print(f'number of rows of df_2: {df_2.shape[0]}')
 
-# We haven't lost any rows here, because .dropna(how='all') drops rows
-# where ALL the values are missing. This dataset has no rows where every
-# value is missing:
-
-print(df.isnull().sum())
